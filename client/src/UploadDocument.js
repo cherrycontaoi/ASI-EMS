f93c862010c645cdfd0adf73030b1aa25ed9d387
cherrycontaoi <cbcontaoi@up.edu.ph>
2024-05-29 00:53:28 +0800

    ui: added rotating asi logo

--- client/src/UploadDocument.js
+++ client/src/UploadDocument.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import logo from "./images/logo-asi.png";
-import logo_gif from "./images/asi-rotating.png";
+import logo_gif from "./images/asi-rotating.gif";
 import "./UploadDocument.css";
 import AdminSignin from "./AdminSignin";
 
